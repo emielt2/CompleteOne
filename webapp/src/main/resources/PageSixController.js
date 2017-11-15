50acb598561adf6d3037f1aeec9cea8ade17f08a
EmielT <emielt@gmail.com>
2017-11-15 16:05:16 +0100

    a

---
+++ webapp/src/main/resources/PageSixController.js
@@ -0,0 +1,15 @@
+var myApp = angular.module('appmodulenameSix', []);
+
+// var pageSixController = function ($scope) {
+//     $scope.message="abcdef"
+// }
+//
+// // myApp.controller('myController', function ($scope) {
+// myApp.controller('pageSixController',pageSixController);
+
+myApp.controller('pageSixController', function ($scope) {
+
+});
+
+
+
